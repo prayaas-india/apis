3fe2efebd700fdb57fc6607a2d38433f9acf8e9d
Hanzala Sohrab <sorb.hanzala@gmail.com>
2021-08-16 22:54:41 +0530

    Added more info about every volunteer

--- app.py
+++ app.py
@@ -5,14 +5,26 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-gc = gspread.service_account(filename="./service_account.json")
+gc = gspread.service_account()
 worksheet = gc.open_by_url("URL").sheet1
 
 
 @app.route("/get2k17", methods=['GET'])
 def get2k17():
-    data = worksheet.col_values(1)[1:]
+    
+    rows = worksheet.get_all_values()[1:]
+    
+    data = []
+    for row in rows:
+        volunteer_info = {
+            "name": row[0],
+            "phone": row[2],
+            "branch": row[3]
+        }
+        data.append(volunteer_info)
+    
     data = json.dumps(data)
+    
     return data
 
 
